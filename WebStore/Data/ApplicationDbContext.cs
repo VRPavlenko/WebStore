dbbb2615c2199071aaadfed4ad5893ec6d5e65dc
VRPavlenko <pavlenkovr@outlook.com>
2022-10-09 02:32:42 +0300

    use nuget entity framework

--- WebStore/Data/ApplicationDbContext.cs
+++ WebStore/Data/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebStore.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
     }
 }
