d16932a9793051c9e5c8a43cdbe8b696be2da4a2
VRPavlenko <pavlenkovr@outlook.com>
2022-10-10 03:00:07 +0300

    Starts filling the page "Context

--- WebStore/Controllers/CategoryController.cs
+++ WebStore/Controllers/CategoryController.cs
@@ -3,14 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) => _db = db;
+        
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
         }
     }
 }
