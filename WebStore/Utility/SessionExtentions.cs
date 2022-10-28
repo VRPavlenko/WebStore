c8949b9dd3a27416c5585f1adde0f9580d3037a3
VRPavlenko <pavlenkovr@outlook.com>
2022-10-29 00:49:22 +0300

    Update session opts, add Cart list into _layout

--- WebStore/Utility/SessionExtentions.cs
+++ WebStore/Utility/SessionExtentions.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace WebStore.Utility
 {
-    public class SessionExtentions
+    //
+    public static class SessionExtentions
     {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var val = session.GetString(key);
+            return val == null ? default : JsonSerializer.Deserialize<T>(val);
+        }
     }
 }
--- WebStore/WC.cs
+++ WebStore/WC.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace WebStore
 {
+    /// <summary>
+    /// WebConst
+    /// </summary>
     public static class WC
     {
-        public static string ImagePath = @"\images\product\"; 
+        public static string ImagePath = @"\images\product\";
+
+        public static string SessionCart = "ShoppingCartSession";
     }
 }
